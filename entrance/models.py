394659dc6205c83b99ddf918ee2eb93719eb8171
biliu33 <1043514482@qq.com>
2019-03-31 14:06:03 +0800

    add user model

--- entrance/models.py
+++ entrance/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=256)
+    cTime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["cTime"]
